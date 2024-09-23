1dbac318c348d83b747e2932252fed31b3b99daa
vp13coder <vpranove@gmail.com>
2024-09-23 11:59:29 +0530

    Add files via upload

---
+++ custom classes in python.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, length: int, width: int):
+        self.length = length
+        self.width = width
+
+    def __iter__(self):
+        yield {'length': self.length}
+        yield {'width': self.width}
+
+
+a = Rectangle(10, 20)
+
+for item in a:
+    print(item)
